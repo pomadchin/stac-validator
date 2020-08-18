d517e7545fd580a3e87136422f177c2d42c38338
jonhealy1 <jonathan.d.healy@gmail.com>
2020-08-18 16:45:30 -0700

    add stac-validator, setuptools

--- cli.py
+++ cli.py
@@ -2,15 +2,23 @@ import click
 import pystac
 import json
 from pystac import Item, Catalog
+from stac_validator import stac_validator
 
 @click.command()
 @click.option('--stac_item', default='sentinel2-sample.json', help="Local stac item to validate")
 def validate_item(stac_item):
     """A simple program that validates a local STAC item."""
+    
+    # pystac validation
     click.echo('Stac_item: %s' % stac_item)
     item = Item.from_file(stac_item)
-    item.validate()
-    print(item)
+    print(item.validate())
+    print(item.bbox)
+
+    # pystac validation
+    stac = stac_validator.StacValidate(stac_item)
+    print(stac.run())
+
 
 if __name__ == '__main__':
     validate_item()
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Stac-Validator-CLI",
+    version="0.1.0",
+    py_modules=['cli'],
+    install_requires=[
+        'click',
+        'pystac[validation]',
+        'stac-validator',
+    ],
+    entry_points='''
+        [console_scripts]
+        cli=cli:validate_item
+    ''',
+)
